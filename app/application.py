e9881c9f06123481d7e4713a9677f8ccf69ee155
Nejc Ambrozic <nejc.ambrozic.slo@gmail.com>
2018-09-05 17:56:18 +0200

    Remove a lot of stuff, basic structure setup

--- app/application.py
+++ app/application.py
@@ -3,32 +3,29 @@ import logging
 import os
 from datetime import datetime
 
-from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, abort, request
-from pytz import utc
 
-import scheduler as sch
 from events import events
 
 logger = logging.getLogger('spam_application')
 logger.setLevel(logging.DEBUG)
 
 # ------------------------------------------------------------------------------
-# apscheduler ------------------------------------------------------------------
-
-scheduler = BackgroundScheduler(timezone=utc)
-try:
-    scheduler.start()
-    sch.add_event_jobs(scheduler)
-    sch.listen_for_contract_events(scheduler)
-except:
-    logger.error("Scheduler error")
-
-# ------------------------------------------------------------------------------
-# flask setup ------------------------------------------------------------------
+# Flask Setup ------------------------------------------------------------------
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+
+def startup():
+    """
+    At startup
+    """
+    print("Validation Node Starting...")
+    all_events = events.get_all_events()
+    my_events = events.get_my_events(all_events)
+    print("Setting up finished")
+
+startup()
 
 
 @application.before_request
@@ -44,9 +41,9 @@ def limit_remote_addr():
         abort(403)
 
 
+
 @application.after_request
 def apply_headers(response):
-
     response.headers["Content-Type"] = "application/json"
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers[
@@ -55,56 +52,27 @@ def apply_headers(response):
         "Access-Control-Allow-Methods"] = "POST,GET,OPTIONS,PUT,DELETE"
     return response
 
-
-# ------------------------------------------------------------------------------
-# flask limiter ----------------------------------------------------------------
-
-#limiter = Limiter(
-#    application,
-#    key_func=get_ipaddr,
-#    default_limits=["200/hour", "60/minute"]
-#)
-
-
-#@limiter.request_filter
+# @limiter.request_filter
+# TODO check why this is here
 def ip_whitelist():
-    #pass
-    return request.remote_addr == "89.212.5.47"
+    return request.remote_addr == os.getenv('IP_WHITELIST')
 
 
 # ------------------------------------------------------------------------------
-# routes -----------------------------------------------------------------------
+# Routes -----------------------------------------------------------------------
 
 
 @application.route('/', methods=['GET'])
-#@limiter.limit("10/minute")
+# @limiter.limit("10/minute")
 def hello():
-    scheduler.print_jobs()
     print("Now: ", datetime.utcnow())
     return "Nothing to see here, verity dev", 200
 
 
 @application.route('/events', methods=['GET'])
 def get_events():
-    include = request.args.get('include')
-    statistics = include == "statistics"
-
-    result = events.get_events_data(stats=statistics)
-
-    if "data" in result:
-        return json.dumps(result), result["data"]["code"]
-    else:
-        return json.dumps(result), result["error"]["code"]
-
-
-@application.route('/events/<int:event_id>', methods=['GET'])
-def get_event_outcome(event_id):
-    result = events.get_event_outcome(event_id)
-
-    if "data" in result:
-        return json.dumps(result), result["data"]["code"]
-    else:
-        return json.dumps(result), result["error"]["code"]
+    my_events = ['abcde']
+    return json.dumps(my_events), 200
 
 
 @application.route('/vote', methods=['POST'])
@@ -116,39 +84,11 @@ def vote():
     if "data" in json_data and "HTTP_X_FORWARDED_FOR" in request.environ:
         json_data["data"]["ip_address"] = request.environ["HTTP_X_FORWARDED_FOR"]
 
-    result = events.vote(json_data, None, scheduler)
+    result = events.vote(json_data)
 
     if "data" in result:
         return json.dumps(result), result["data"]["code"]
     return json.dumps(result), result["error"]["code"]
-
-
-# ------------------------------------------------------------------------------
-# DEV endpoints ----------------------------------------------------------------
-@application.route('/kill_event', methods=['POST'])
-def kill_event():
-    json_data = request.get_json()
-    headers = request.headers
-
-    result = events.kill_event(json_data, scheduler)
-
-    if "data" in result:
-        return json.dumps(result), result["data"]["code"]
-    else:
-        return json.dumps(result), result["error"]["code"]
-
-
-@application.route('/reload_jobs', methods=['POST'])
-def reload_jobs():
-    json_data = request.get_json()
-    headers = request.headers
-
-    result = events.reload_jobs(json_data, scheduler)
-
-    if "data" in result:
-        return json.dumps(result), result["data"]["code"]
-    else:
-        return json.dumps(result), result["error"]["code"]
 
 
 # run the app.
--- app/common.py
+++ app/common.py
@@ -3,8 +3,6 @@ import random
 import string
 import time
 
-import requests
-
 
 def random_string(size=11, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in list(range(size)))
@@ -63,41 +61,3 @@ def pad_hex_right(string, num_of_bytes, prefix=False):
     else:
         return string[2:].ljust(num_of_bytes*2, '0')
 
-def send_terminal_event(event_id, text):
-
-    headers = {
-        "Content-Type": "application/json",
-    }
-
-    # TODO Nejc: move this api key to env
-    data = {
-        "api_key": "***REMOVED***",
-        "event": {
-            "event_id": event_id,
-            "text": text
-        }
-    }
-
-    r = requests.post("https://socket.eventum.network/status", headers = headers, json = data)
-
-def send_terminal_join_close(event_id, contract_address, start_time):
-    print("SEND TERMINAL EVENT CALLED")
-
-    message = "Time to join ended for event " + contract_address
-    send_terminal_event(event_id, message)
-
-    message = "Selecting nodes..."
-    send_terminal_event(event_id, message)
-
-    time.sleep(1)
-
-    message = "Nodes selected: [5da6b2, 069b85, 317c77]"
-    send_terminal_event(event_id, message)
-
-    time.sleep(1)
-
-    message = "Nodes synced and ready"
-    send_terminal_event(event_id, message)
-
-    message = "Waiting for event " + contract_address + " to start @ " + str(start_time)
-    send_terminal_event(event_id, message)
