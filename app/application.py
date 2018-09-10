2e6d3ebc897b9edf12a4f8aa236d565fe499d1bb
Roman Orac <orac.roman@gmail.com>
2018-09-10 14:46:09 +0200

    Store events in redis

--- app/application.py
+++ app/application.py
@@ -1,11 +1,11 @@
+import functools
 import json
 import logging
 import os
 from datetime import datetime
 
-import functools
 import redis
-from flask import Flask, abort, request, jsonify
+from flask import Flask, abort, jsonify, request
 
 from events import events
 
@@ -20,28 +20,45 @@ application.logger.handlers.extend(gunicorn_error_logger.handlers)
 application.logger.setLevel(logging.DEBUG)
 logger = application.logger
 
-#r = redis.StrictRedis(host='redis', port=6379, db=0)
+# redis_db = redis.StrictRedis(host='redis', port=6379, db=0)
 redis_db = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+
+EVENTS_KEY = 'events'
 
 
 def init():
     logger.info('Validation Node Init started')
 
-    redis_db.set('foo', 'bar')
-
     all_events = events.all_events_addresses()
     logger.info('All event addresses %s', all_events)
 
-    node_events = events.filter_node_events(all_events)
-    logger.info('Node event addresses %s', node_events)
+    filtered_events = events.filter_events_addresses(all_events)
+    logger.info('Filtered event addresses %s', filtered_events)
+
+    node_events = events.retrieve_events(filtered_events)
+    logger.info('Validation node events %s', node_events)
+
+    for event in node_events:
+        event_json = event.to_json()
+        redis_db.rpush(EVENTS_KEY, event_json)
+
+    node_events = []
+    for i in range(redis_db.llen(EVENTS_KEY)):
+        event_json = redis_db.lindex(EVENTS_KEY, i)
+        event = events.Event.from_json(event_json)
+        node_events.append(event)
+    logger.info('Validation node events in redis %s', node_events)
 
     logger.info('Validation Node Init done')
+
 
 def return_json(resp):
     @functools.wraps(resp)
     def wrapped_resp(**values):
         return jsonify(resp(**values))
+
     return wrapped_resp
+
 
 @application.before_request
 def limit_remote_addr():
