85b9221afd7ca0c1081d665f68d0631d4d7db1be
Nejc Ambrozic <nejc.ambrozic.slo@gmail.com>
2018-09-13 14:02:10 +0200

    Remove loggs from filter.

--- app/events/filters.py
+++ app/events/filters.py
@@ -8,10 +8,10 @@ logger = logging.getLogger('flask.app')
 def filter_join_events(w3, contract_abi):
     node_events = events.get_all_events()
     for event in node_events:
+        # FIXME until state is managed skip this
         #if event.state != 1:
         #    continue
         contract_instance = w3.eth.contract(address=event.event_address, abi=contract_abi)
-        logger.info('FILTERS  %s' % event.event_address)
         event_filter = contract_instance.events.JoinEvent.createFilter(
             fromBlock='earliest', toBlock='latest')
         entries = event_filter.get_all_entries()
