6e9ede4bffbee4aa5b43dd5efd17b504c6659b70
Roman Orac <orac.roman@gmail.com>
2018-09-20 15:04:37 +0200

    Improve logging

--- app/ethereum/rewards.py
+++ app/ethereum/rewards.py
@@ -30,22 +30,22 @@ def determine_rewards(event_id, consensus_votes):
 
 def set_consensus_rewards(event_id):
     w3 = EthProvider().web3()
-    logger.info('Setting rewards for %s started', event_id)
+    logger.info('Started setting rewards for %s', event_id)
     user_ids, eth_rewards, token_rewards = database_events.Rewards.get_lists(event_id)
     contract_abi = common.verity_event_contract_abi()
 
     contract_instance = w3.eth.contract(address=event_id, abi=contract_abi)
     contract_instance.functions.setRewards(user_ids, eth_rewards, token_rewards).transact()
-    logger.info('Setting rewards for %s done', event_id)
+    logger.info('Finished setting rewards for %s', event_id)
 
     mark_rewards_set(contract_instance, event_id, user_ids, eth_rewards, token_rewards)
 
 
 def mark_rewards_set(contract_instance, event_id, user_ids, eth_rewards, token_rewards):
-    logger.info('Marking rewards for %s started', event_id)
+    logger.info('Started marking rewards for %s', event_id)
     rewards_hash = database_events.Rewards.hash(user_ids, eth_rewards, token_rewards)
     contract_instance.functions.markRewardsSet(rewards_hash).transact()
-    logger.info('Marking rewards for %s done', event_id)
+    logger.info('Finished marking rewards for %s', event_id)
 
 
 def validate_rewards(event_id, validation_round):
