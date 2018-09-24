51523f2bf3f611aa25e4c810863022d947a3d5fe
Roman Orac <orac.roman@gmail.com>
2018-09-24 12:09:52 +0200

    Fix response status

--- app/common.py
+++ app/common.py
@@ -1,28 +1,5 @@
 import json
 import os
-import time
-
-
-def calculate_event_state(end_flag, time_to_join, start_time, end_time, reward_claimable):
-    current_timestamp = int(time.time())
-
-    if end_flag:
-        state = 3
-        if reward_claimable:
-            state = 4
-    else:
-        if current_timestamp < start_time and current_timestamp < time_to_join:
-            state = 0  # upcoming joinable
-        elif current_timestamp < start_time and current_timestamp >= time_to_join:
-            state = 1  # upcoming unjoinable
-        elif current_timestamp >= start_time and current_timestamp < end_time:
-            state = 2  # live
-        else:
-            state = 3  # done
-            if reward_claimable:
-                state = 4
-
-    return state
 
 
 def verity_event_contract_abi():
