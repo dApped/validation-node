cd193b4db7e94a6c2c5df24dffd0586e8f3c2436
Nejc Ambrozic <nejc.ambrozic.slo@gmail.com>
2018-11-05 15:12:53 +0100

    Update env files.

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,6 @@
 import hashlib
 import json
 import logging
-import os
 from collections import OrderedDict, defaultdict
 
 import redis
@@ -10,8 +9,7 @@ import common
 
 logger = logging.getLogger('flask.app')
 
-redis_db = redis.StrictRedis(
-    host=os.getenv('REDIS_URL', '127.0.0.1'), port=6379, db=0, decode_responses=True)
+redis_db = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
 
 
 def flush_database():
