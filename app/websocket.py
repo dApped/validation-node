a8a9eb2868270027d78ba3152f7ff131cb2645e2
Roman Orac <orac.roman@gmail.com>
2018-11-12 15:26:39 +0100

    Update websocket port

--- app/websocket.py
+++ app/websocket.py
@@ -51,7 +51,7 @@ class Common:
         for node_ip_port in node_ips_ports:
             if my_ip_port == node_ip_port:
                 continue
-            websocket_address = '%s86' % node_ip_port  # TODO remove this in production
+            websocket_address = node_ip_port.split(':')[0] + ':8765'
             websocket = await cls.get_or_create_websocket_connection(websocket_address)
             websockets_nodes.append(websocket)
         return websockets_nodes
