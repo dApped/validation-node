3cabdf96bb451919cae5da5c30426165282b92ea
Roman Orac <orac.roman@gmail.com>
2018-11-19 10:05:24 +0100

    Add more logs when calculating consensus

--- app/events/consensus.py
+++ app/events/consensus.py
@@ -10,8 +10,19 @@ logger = logging.getLogger('flask.app')
 
 def should_calculate_consensus(event, vote_count):
     '''Heuristic which checks if there is a potential for consensus (assumes all votes are valid)'''
+    event_id = event.event_id
     participant_ratio = (vote_count / len(event.participants())) * 100
-    return vote_count >= event.min_total_votes and participant_ratio >= event.min_participant_ratio
+    if vote_count < event.min_total_votes:
+        logger.info('[%s] Should not calculate consensus: vote_count<min_total_votes: %d<%d',
+                    event_id, vote_count, event.min_total_votes)
+        return False
+    if participant_ratio < event.min_participant_ratio:
+        logger.info(
+            '[%s] Should not calculate consensus: participant_ratio<min_participant_ratio: %d<%d',
+            event_id, vote_count, event.min_total_votes)
+        return False
+    logger.info('[%s] Should calculate consensus', event_id)
+    return True
 
 
 def check_consensus(event, event_metadata):
@@ -20,15 +31,14 @@ def check_consensus(event, event_metadata):
     vote_count = len(votes_by_users)
 
     if not should_calculate_consensus(event, vote_count):
-        logger.info('[%s] Should not calculate consensus for event', event_id)
         return
     consensus_votes_by_users = calculate_consensus(event, votes_by_users)
     if not consensus_votes_by_users:
-        logger.info('[%s] Consensus not reached for event', event_id)
+        logger.info('[%s] Consensus not reached', event_id)
         return
-    logger.info('[%s] Consensus reached for event', event_id)
+    logger.info('[%s] Consensus reached', event_id)
     if event.metadata().is_consensus_reached:
-        logger.info('[%s] Consensus already set for event', event_id)
+        logger.info('[%s] Consensus already set', event_id)
         return
     event_metadata.is_consensus_reached = True
     event_metadata.update()
@@ -38,14 +48,14 @@ def check_consensus(event, event_metadata):
     if event.is_master_node:
         scheduler.scheduler.add_job(rewards.set_consensus_rewards, args=[NODE_WEB3, event_id])
     else:
-        logger.info('Not a master node for %s event. Waiting for rewards to be set.', event_id)
+        logger.info('[%s] Not a master node. Waiting for rewards to be set.', event_id)
 
 
 def calculate_consensus(event, votes_by_users):
     vote_count = len(votes_by_users)
     if vote_count < event.min_total_votes:
         logger.info(
-            '[%s] Not enough valid votes to calculate consensus for event. votes_by_users=%d, min_total_votes=%d',
+            '[%s] Not enough valid votes to calculate consensus. votes_by_users=%d, min_total_votes=%d',
             event.event_id, len(votes_by_users), event.min_total_votes)
         return dict()
 
@@ -62,7 +72,7 @@ def calculate_consensus(event, votes_by_users):
     if (consensus_votes_count < event.min_consensus_votes
             or consensus_ratio * 100 < event.min_consensus_ratio):
         logger.info(
-            '[%s] Not enough consensus votes for event. votes_by_users=%d, min_total_votes=%d, consensus_ratio=%d, min_consensus_ratio=%d',
+            '[%s] Not enough consensus votes. votes_by_users=%d, min_total_votes=%d, consensus_ratio=%d, min_consensus_ratio=%d',
             event.event_id, len(votes_by_users), event.min_total_votes, consensus_ratio,
             event.min_consensus_ratio)
         return dict()
