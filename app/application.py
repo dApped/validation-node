7d265f367195b730084aa9f60c00c79e0a991462
Roman Orac <roman.o@verity.network>
2019-02-28 10:59:47 +0100

    Add HTTP_X_FORWARDED_FOR header

--- app/application.py
+++ app/application.py
@@ -81,7 +81,7 @@ def limit_remote_addr():
 def apply_headers(response):
     response.headers['Content-Type'] = 'application/json'
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,X-Forwarded-For'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,X-Forwarded-For,HTTP_X_FORWARDED_FOR'
     response.headers['Access-Control-Allow-Methods'] = 'POST,GET'
     return response
 
