f0c85eaa49635f51ffcd0d12834508ebd53470fc
Roman Orac <roman.o@verity.network>
2019-03-20 14:40:44 +0100

    Add gas estimate

--- app/queue_service/transactions.py
+++ app/queue_service/transactions.py
@@ -11,6 +11,7 @@ from queue_service.queue_service import QUEUE_IN, QUEUE_OUT
 logger = logging.getLogger()
 
 GAS_PRICE_FACTOR = 1.2
+GAS_LIMIT_FACTOR = 2
 WAIT_FOR_TRANSACTION_RECEIPT_TIMEOUT = 60 * 15  # 15 minutes
 
 
@@ -19,20 +20,26 @@ def _raw_transaction(w3, contract_function, account, gas_price, nonce):
         'from': account['address'],
         'nonce': nonce,
     }
+    gas = _estimate_gas_limit(contract_function)
     transaction['gasPrice'] = gas_price
-    transaction['gas'] = 2000000
+    transaction['gas'] = gas
     signed_txn = w3.eth.account.signTransaction(
         contract_function.buildTransaction(transaction), private_key=account['pvt_key'])
     raw_txn = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    logger.info('Raw transaction nonce: %d, gas_price: %d, gas: %d', nonce, gas_price, gas)
     return raw_txn
 
 
-def estimate_gas_price(w3, wei_addition=0):
+def _estimate_gas_limit(contract_function):
+    return int(GAS_LIMIT_FACTOR * contract_function.estimateGas())
+
+
+def _estimate_gas_price(w3, wei_addition=0):
     w3.eth.setGasPriceStrategy(fast_gas_price_strategy)
     return int(GAS_PRICE_FACTOR * w3.eth.generateGasPrice() + wei_addition)
 
 
-def get_nonce(w3, address, max_retries=3):
+def _next_nonce(w3, address, max_retries=3):
     for attempt in range(max_retries):
         try:
             return w3.eth.getTransactionCount(address)
@@ -43,18 +50,16 @@ def get_nonce(w3, address, max_retries=3):
     return None
 
 
-def function_transact(w3, contract_function, max_retries=3):
+def _function_transact(w3, contract_function, max_retries=3):
     account = node_key_store.account_dict()
 
     for retry in range(max_retries):
-        next_nonce = get_nonce(w3, account['address'])
+        next_nonce = _next_nonce(w3, account['address'])
         if next_nonce is None:
             return None
-
         gas_price_addition = web3.Web3.toWei(retry, 'gwei')
-
         try:
-            gas_price = estimate_gas_price(w3, wei_addition=gas_price_addition)
+            gas_price = _estimate_gas_price(w3, wei_addition=gas_price_addition)
             tx_raw = _raw_transaction(w3, contract_function, account, gas_price, next_nonce)
             tx_receipt = w3.eth.waitForTransactionReceipt(
                 tx_raw, timeout=WAIT_FOR_TRANSACTION_RECEIPT_TIMEOUT)
@@ -74,7 +79,7 @@ def function_transact(w3, contract_function, max_retries=3):
 
 
 def queue_transaction(w3, contract_function, event_id='default'):
-    QUEUE_IN.put((function_transact, w3, contract_function))
+    QUEUE_IN.put((_function_transact, w3, contract_function))
     QUEUE_IN.join()
     while True:
         result = QUEUE_OUT.get(block=False)
